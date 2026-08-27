b36782e5d5409ad176975659dea562c6670e1c21
udsinghh <uditisingh209@gmail.com>
2026-08-27 17:13:35 +0530

    Update create-repo.sh

--- scripts/create-repo.sh
+++ scripts/create-repo.sh
@@ -6,10 +6,7 @@ TEAM="team-a"
 
 echo "Creating repository: $REPO_NAME"
 
-# Authenticate Git using GitHub token
-gh auth setup-git
-
-# Create private repository inside the organization
+# Create private repository inside organization
 gh repo create "$ORG/$REPO_NAME" --private
 
 # Add Team-A with Read permission
@@ -20,7 +17,46 @@ gh api --method PUT \
 # Clone the newly created repository
 gh repo clone "$ORG/$REPO_NAME"
 
-# Go inside the new repository
+# Go inside the repository
+cd "$REPO_NAME"
+
+# Copy template files
+cp ../template-files/* .
+
+# Configure Git
+git config user.name "udsinghh"
+git config user.email "uditisingh1999@gmail.com"
+
+# Create main branch
+git branch -M main
+
+# Add files
+git add .
+
+# Commit files
+git commit -m "Add template files"
+
+# Push files
+git push -u origin main#!/bin/bash
+
+REPO_NAME=$1
+ORG="repo-automation-org"
+TEAM="team-a"
+
+echo "Creating repository: $REPO_NAME"
+
+# Create private repository inside organization
+gh repo create "$ORG/$REPO_NAME" --private
+
+# Add Team-A with Read permission
+gh api --method PUT \
+  "/orgs/$ORG/teams/$TEAM/repos/$ORG/$REPO_NAME" \
+  -f permission=pull
+
+# Clone the newly created repository
+gh repo clone "$ORG/$REPO_NAME"
+
+# Go inside the repository
 cd "$REPO_NAME"
 
 # Copy template files
